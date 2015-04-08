4433b67432d178f3188030d276005aa8fd592421
Dieter Plaetinck <dieter@vimeo.com>
2015-04-08 13:35:12 -0400

    for large amount of targets, request in chunks

--- web.go
+++ web.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"net/http"
 	"net/url"
+	"sync"
 	"time"
 )
 
@@ -18,29 +19,29 @@ type Request struct {
 }
 
 type Graphite struct {
-    u url.URL
-    httpClient *http.Client
+	u          url.URL
+	httpClient *http.Client
 }
 
 func NewUrl(urlString string) (*Graphite, error) {
-    url, err := url.Parse(urlString)
-    if err != nil {
-        return nil,err
-    }
-    return &Graphite{*url,&http.Client{}}, nil
+	url, err := url.Parse(urlString)
+	if err != nil {
+		return nil, err
+	}
+	return &Graphite{*url, &http.Client{}}, nil
 }
 func New(scheme, host, path string) *Graphite {
-    if scheme == "" {
-        scheme = "http"
-    }
-    return &Graphite{
-        url.URL{
-            Scheme: scheme,
-            Host: host,
-            Path: path,
-        },
-        &http.Client{},
-    }
+	if scheme == "" {
+		scheme = "http"
+	}
+	return &Graphite{
+		url.URL{
+			Scheme: scheme,
+			Host:   host,
+			Path:   path,
+		},
+		&http.Client{},
+	}
 }
 
 type RenderResponse []Series
@@ -54,8 +55,8 @@ type Series struct {
 type DataPoint []json.Number
 
 func (g *Graphite) Metrics() (metrics MetricsResponse, err error) {
-    u := g.u
-    u.Path += "/metrics/index.json"
+	u := g.u
+	u.Path += "/metrics/index.json"
 
 	resp, err := g.httpClient.Get(u.String())
 	if err != nil {
@@ -69,10 +70,82 @@ func (g *Graphite) Metrics() (metrics MetricsResponse, err error) {
 	return
 }
 
+func queryWorker(wg *sync.WaitGroup, reqChan chan *Request, respChan chan *RenderResponse, errChan chan error, g *Graphite) {
+	for r := range reqChan {
+		resp, err := g.Query(r)
+		respChan <- &resp
+		errChan <- err
+	}
+	wg.Done()
+}
 
 func (g *Graphite) Query(r *Request) (RenderResponse, error) {
-    u := g.u
-    u.Path += "/render"
+	if len(r.Targets) > 20 {
+		workers := 10
+		reqChan := make(chan *Request)
+		respChan := make(chan *RenderResponse)
+		errChan := make(chan error)
+
+		// start workers
+		wg := sync.WaitGroup{}
+		for i := 0; i < workers; i++ {
+			wg.Add(1)
+			go queryWorker(&wg, reqChan, respChan, errChan, g)
+		}
+
+		stopFeeding := make(chan struct{})
+		// start sending jobs to workers in background
+		go func(start, end *time.Time, targets []string) {
+			for {
+				upper := 20
+				if upper > len(targets) {
+					upper = len(targets)
+				}
+				section := targets[0:upper]
+				select {
+				case <-stopFeeding:
+					close(reqChan)
+					return
+				default:
+					reqChan <- &Request{
+						start,
+						end,
+						section,
+					}
+				}
+				if len(targets) > 20 {
+					targets = targets[20:]
+				} else {
+					break
+				}
+			}
+			close(reqChan)
+		}(r.Start, r.End, r.Targets)
+
+		totalResp := *new(RenderResponse)
+		go func() {
+			wg.Wait()
+			fmt.Println("workers done")
+			close(respChan)
+			close(errChan)
+		}()
+		for {
+			resp, ok := <-respChan
+			err := <-errChan
+			if err != nil {
+				stopFeeding <- struct{}{}
+				return RenderResponse{}, err
+			}
+			if !ok {
+				break
+			}
+			totalResp = append(totalResp, *resp...)
+		}
+		return totalResp, nil
+	}
+
+	u := g.u
+	u.Path += "/render"
 	v := url.Values{
 		"format": []string{"json"},
 		"target": r.Targets,
@@ -83,26 +156,18 @@ func (g *Graphite) Query(r *Request) (RenderResponse, error) {
 	if r.End != nil {
 		v.Add("until", fmt.Sprint(r.End.Unix()))
 	}
-    var resp *http.Response
-    var err error
-    // there's a max url length, so we can only use GET up to a certain point.
-    if len(v["target"]) < 30 {
-        u.RawQuery= v.Encode()
-        resp, err = g.httpClient.Get(u.String())
-    } else {
-        resp, err = g.httpClient.PostForm(u.String(), v)
-        fmt.Println("PostForm err", err)
-    }
+	var resp *http.Response
+	var err error
+	u.RawQuery = v.Encode()
+	resp, err = g.httpClient.Get(u.String())
 	if err != nil {
 		return RenderResponse{}, err
 	}
 	defer resp.Body.Close()
 	if resp.StatusCode != http.StatusOK {
-        fmt.Println("statuscode err", resp.Status)
 		return RenderResponse{}, errors.New(resp.Status)
 	}
 	var series RenderResponse
 	err = json.NewDecoder(resp.Body).Decode(&series)
-    fmt.Println("json decode err", err)
 	return series, err
 }
